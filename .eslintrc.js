043cc252501f5274535f0bad35fd14fe65a867d6
CNine <cnine229@gmail.com>
2020-12-29 22:20:47 +0800

    chore: remove useless eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
   rules: {
     'prettier/prettier': 'warn',
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
   },
   ignorePatterns: ['public/', 'dist/'],
 }

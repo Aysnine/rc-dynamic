95798fea38165b82e9abff296f1a0404067e97d2
CNine <cnine229@gmail.com>
2020-12-29 21:56:18 +0800

    chore: better eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  extends: ['react-app', 'react-app/jest'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off'
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
   },
   ignorePatterns: ['public/', 'dist/']
 }

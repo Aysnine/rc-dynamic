a0193310a997547be08ca98f3e46b79e170c6232
CNine <cnine229@gmail.com>
2021-01-08 14:34:13 +0800

    feat: clear and reset default

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { useEffect, useRef, useState } from 'react'
-import { useUpdate } from 'react-use'
+import { useCallback, useEffect, useRef } from 'react'
+import { useLocalStorage, useUpdate } from 'react-use'
 import { DynamicTreeNode } from './components'
 import TreeRoot from './components/core/TreeRoot'
 import { mockTree } from './mockData'
 
 const App = () => {
-  const [tree, setTree] = useState<DynamicTreeNode[]>(mockTree)
-  const [activeId, setActiveId] = useState<string>('2')
+  const [tree, setTree] = useLocalStorage<DynamicTreeNode[]>('tree', mockTree)
+  const [activeId, setActiveId] = useLocalStorage<string>('activeId', '2')
+
+  const handleResetDefault = useCallback(() => {
+    setTree(mockTree)
+    setActiveId('2')
+  }, [setActiveId, setTree])
+
+  const handleClear = useCallback(() => {
+    setTree([])
+    setActiveId('')
+  }, [setActiveId, setTree])
 
   // ! force render for portal
   const panel = useRef<HTMLDivElement>(null)
@@ -18,8 +28,15 @@ const App = () => {
   return (
     <div className="workspace">
       <div className="material-panel">
-        TODO:
-        <p>components panel here</p>
+        <div>
+          <button onClick={handleClear}>clear</button>
+          &nbsp;
+          <button onClick={handleResetDefault}>reset default</button>
+        </div>
+        <p>
+          <b>Components</b>
+        </p>
+        <p>TODO</p>
       </div>
       <div className="view-container">
         <TreeRoot tree={tree} setTree={setTree} activeId={activeId} setActiveId={setActiveId} panel={panel} />
